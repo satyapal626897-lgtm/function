87c62511416f125a73a722f46221f8f954894492
satyapal626897-lgtm <satyapal626897@gmail.com>
2025-12-04 17:05:14 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,62 @@
+
+// let box = document.querySelector("#sattu")
+
+
+// let red =() => {
+
+//     box.style.backgroundColor = "red"
+
+// }
+
+// let green =() => {
+
+//     box.style.background = "green";
+
+// }
+
+// let blue=() => {
+
+//     box.style.background = "blue";
+// }
+
+
+
+// let mytext=document.querySelector("#mytext")
+
+
+// let hidd=()=>
+
+//     let mytext=document.querySelector('#mytext')
+
+//     mytext.style.display=('none')
+ 
+
+
+// let toggle=()=>{
+
+//     let okk=document.querySelector("#te");
+
+//     if(okk.style.display=="block"){
+
+//         okk.style.display="none"
+//     }else{
+
+//         okk.style.display="block"
+//     }
+    
+// }
+
+
+
+let tool=()=>{
+
+    let okk=document.querySelector('#box');
+
+    okk.style.backgroundImage="url(image.png)"
+
+
+
+}
+
+
+
